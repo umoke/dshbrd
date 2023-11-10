4f07d49787767e2fc1017b4ae6734da500fd9b7e
umoke <57583881+umoke@users.noreply.github.com>
2023-11-10 16:29:34 +1000

    Добавление круговой диаграммы

--- db.py
+++ db.py
@@ -16,11 +16,15 @@ dropdown = dcc.Dropdown(
 time_series_chart = dcc.Graph(
 )
 
+pie_chart = dcc.Graph(
+)
+
 app.layout = html.Div([
     html.H1(children='Анализ данных о посещении веб-сайта'),
     data_table,
     dropdown,
     time_series_chart,
+    pie_chart,
 ])
 
 if __name__ == '__main__':
