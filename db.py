c109977fea30959137a27765faf77f6c80ef3847
umoke <57583881+umoke@users.noreply.github.com>
2023-11-10 14:47:07 +1000

    Добавление источника данных

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
 import dash
 from dash import html
+import pandas as pd
 
 app = dash.Dash(__name__)
 
+df = pd.read_csv('data/visitors.csv')
+
 app.layout = html.Div([
-    html.H1('Анализ данных о посещении веб-сайта'),
+    html.H1(children='Анализ данных о посещении веб-сайта'),
+
 ])
 
 if __name__ == '__main__':
