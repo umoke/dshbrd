5205670e524b4676db52cc22c463b9eb34f55230
umoke <57583881+umoke@users.noreply.github.com>
2023-11-10 16:15:48 +1000

    Добавление таблицы

--- db.py
+++ db.py
@@ -1,14 +1,18 @@
 import dash
-from dash import html
+from dash import html, dash_table
 import pandas as pd
 
 app = dash.Dash(__name__)
 
 df = pd.read_csv('data/visitors.csv')
 
+data_table = dash_table.DataTable(
+    data=df.to_dict('records')
+)
+
 app.layout = html.Div([
     html.H1(children='Анализ данных о посещении веб-сайта'),
-
+    data_table
 ])
 
 if __name__ == '__main__':
